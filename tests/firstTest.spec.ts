62a200d8688786a7ddd9d7b3b2fcdd77f9a56798
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2026-02-13 13:59:01 +0200

    User-facing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -35,5 +35,15 @@ test('Locator syntax rules', async ({ page }) => {
 
     // By exact text match
     page.locator(':text-is("Exact full text here")')
+})
 
+
+test.only('User-facing locators', async ({ page }) => {
+    await page.getByRole('textbox', { name: "Email" }).first().click()
+    await page.getByRole('button', { name: "Sign in" }).first().click()
+    await page.getByLabel('Email').first().click()
+    await page.getByPlaceholder('Jane Doe').click()
+    await page.getByText('Using the Grid').click()
+    await page.getByTitle('IoT Dashboard').click()
+    await page.getByTestId('directions').click() // data-testid="directions"
 })
