57f79ed0e8e69046572e61bc859f2d833ed6e2cd
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2026-02-13 16:52:06 +0200

    Extracting values (text, array of texts, input vanue, attribute)

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -94,3 +94,25 @@ test('Reusing locators', async ({ page }) => {
 
     await expect(emailField).toHaveValue('test@test.com')
 })
+
+
+test('Extracting values', async ({ page }) => {
+    // single text value
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic form" })
+    const buttonText = await basicForm.locator('button').textContent()
+    expect(buttonText).toEqual('Submit')
+
+    // all text values
+    const allRadioButtonLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonLabels).toContain('Option 1')
+
+    // input value
+    const emailField = basicForm.getByRole('textbox', { name: 'Email' })
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    // value of attribute
+    const placeHolderValue = await emailField.getAttribute('placeholder')
+    expect(placeHolderValue).toEqual('Email')
+})
