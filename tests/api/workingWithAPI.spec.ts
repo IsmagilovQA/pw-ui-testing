8d17fdeb322db3e127cebcc45855ffdb7794f3bd
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2026-02-17 22:44:01 +0200

    Modify API response

--- tests/api/workingWithAPI.spec.ts
+++ tests/api/workingWithAPI.spec.ts
@@ -10,10 +10,24 @@ test.beforeEach(async ({ page }) => {
         })
     })
 
+    // Modify API response
+    await page.route('*/**/api/articles*', async route => {
+        const response = await route.fetch()
+        const responseBody = await response.json()
+        responseBody.articles[0].title = 'This is modified title from API'
+        responseBody.articles[0].description = 'This is modified description from API'
+
+        await route.fulfill({
+            body: JSON.stringify(responseBody)
+        })
+    })
+
     await page.goto('https://conduit.bondaracademy.com/')
 })
 
 test('mock API test', async ({ page }) => {
     await expect(page.locator('.navbar-brand')).toHaveText('conduit')
     await expect(page.locator('.sidebar')).toContainText('mock')
+    await expect(page.locator('app-article-list h1').first()).toContainText('This is modified title from API')
+    await expect(page.locator('app-article-list p').first()).toContainText('This is modified description from API')
 })
